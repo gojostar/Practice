fb958974457924ac01b21f8794fb441ecb00490b
Mohit Yadav <mohitcodes99@gmail.com>
2023-09-19 15:33:55 +0530

    modify after wsl2

--- temp_converter.c
+++ temp_converter.c
@@ -6,7 +6,7 @@ int main()
     printf(" Enter the temperature in Celcius: \n"); // ask for input
     scanf("%f", &celcius); // taking input
     fahrenheit = (celcius*1.8) + 32; // calculate value in fahrenheit
-    printf("The value of temperature in Fahrenheit is %f and celcius is %f", fahrenheit, celcius); //print value in fahrenheit and celcius
+    printf("The value of temperature in Fahrenheit is %f and celcius is\n %f", fahrenheit, celcius); //print value in fahrenheit and celcius
     return 0; //terminate program
 
 }
