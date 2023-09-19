a784e4c32d03cece5565ed24d472e6da38635d7d
Mohit Yadav <mohitcodes99@gmail.com>
2023-09-19 23:31:15 +0530

    add if_statement.c

---
+++ if_statement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int a;
+printf("Enter a number: \n");
+scanf("%d", &a);
+
+if (a > 0)
+
+{
+    printf("Number is Positive.\n");
+}
+    
+printf("Rest of the Program.\n");
+return 0;
+
+}
