53e0c1b86336605ee3c9e4d4553e58da670ba2c2
Leosword-101 <mohitcodes99@gmail.com>
2023-09-27 00:04:53 +0530

    added while_loop.c

---
+++ while_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, sum = 0;
+
+    while(a < 8)
+    {
+         printf("hello world\n");
+         a++;
+    }
+
+    printf("End of loop\n");
+    return 0;
+
+}
