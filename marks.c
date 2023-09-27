53a1ff425a599b4854c722db6c84a2cd763e5771
Leosword-101 <mohitcodes99@gmail.com>
2023-09-27 17:10:01 +0530

    added a new feature in average calculator

--- marks.c
+++ marks.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
-#define N 4
 
 int main ()
-{
-    int index = 1;
-    while(index <= N)
+{   printf("\nWelcome to the Average marks calculator !\n");
+    printf("\nThis Program is made by Mohit Yadav.\n");
+    int total_students;
+    int total_subjects;
+    int student_num = 1;
+    printf("\nEnter total number of students: \n");
+    scanf("%d", &total_students);
+    printf("\nEnter total number of subjects: \n");
+    scanf("%d", &total_subjects);
+
+    while( student_num <= total_students)
     {
+        printf("\nFor student no: %d \n", student_num);
         float avg;
         int sum = 0;
         int marks;
-        int j = 1;
-        while (j <= 3)
+        int subject = 1;
+        
+        while (subject <= total_subjects)
         {
-            printf("Enter marks for Subject %d: \n", j);
+            printf("\nEnter marks for Subject %d: \n", subject);
             scanf("%d", &marks);
             sum += marks;
-            j++;
+            subject++;
         }
-        avg = sum / 3.0;
-        printf("Average of student %d is %f\n", index, avg);
-        index++;
+        avg = sum / total_subjects;
+        printf("\nAverage of student %d is %f\n", student_num, avg);
+        student_num++;
     }
+        printf("\nThanks for using Average Marks Calculator !\n");
         return 0;
 }
