59c84890395a03681c1b219f72096381161227cb
Leosword-101 <mohitcodes99@gmail.com>
2023-10-06 22:22:07 +0530

    Added For_loop.c

---
+++ for_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, sum = 0;
+
+    for (i=1; i<=1000; i++)
+    {
+        if ((i%3==0) && (i%4!=0))
+        sum += i;
+ 
+    }
+   printf("\nRequired Output = %d\n", sum);
+    return 0;
+}
