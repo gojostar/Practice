b17eebf61b6deb34175747900aa4aaf5a77efd6d
Leosword-101 <moitcodes99@gmail.com>
2023-10-08 21:12:40 +0530

    Added prime number check program

---
+++ prime.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void isPrime(int n);
+
+void isPrime(int n)
+{
+    int i;
+    int notPrimeFlag = 0;
+    for (i = 2; i < n; i++)
+    {
+        if (n%i == 0)
+        {
+            notPrimeFlag = 1;
+            break;
+        }
+    }
+    if (notPrimeFlag == 1)
+        printf("n is not a prime number.\n");
+    else
+        printf("n is a prime number.\n");
+    return;
+
+}
+
+int main()
+{
+    printf("Enter a number for primality test: ");
+    int n;
+    scanf("%d", &n);
+    isPrime(n);
+}
