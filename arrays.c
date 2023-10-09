b8d7e6c79b282462215d056f0dba3826be258c76
Leosword-101 <moitcodes99@gmail.com>
2023-10-09 12:53:23 +0530

    added arrays.c

---
+++ arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers[5] = {1, 2, 3, 4, 5};
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d ", numbers[i]);
+    }
+    return 0;
+}
